fb0cfa5adb8a318c3ba4d63dda0d78d94593823f
Keven de Souza <kevensouza8000@gmail.com>
2024-09-02 23:05:08 -0300

    aula 16

---
+++ CursoPython_Mundo3/Aula16_Tuplas/Aula16.py
@@ -0,0 +1,21 @@
+# tuplas são IMUTÀVEIS
+lanche = ('arroz', 'suco', 'pizza', 'feijão')
+'''
+while True:
+    for comida in lanche:
+        print(f'eu vou cumerrr {comida}')
+    if comida == 'feijão':
+        break
+for c in range(0 , len(lanche)):
+    print(f'eu vou cumerrr {lanche[c]}')
+
+for posição, comida in enumerate(lanche):
+    print(f'eu vou cumerrr {comida} que está na posição {posição}')'''
+
+a = (1, 5, 6)
+b = (4, 6, 7, 8)
+c = a + b
+print (max(c))
+print (min(c))
+print (f'Quantas vezes aparece o número 6? aparece : {c.count(6)} vezes')
+print (f'Qual posição está o 4? aparece na posição {c.index(4)} ')
